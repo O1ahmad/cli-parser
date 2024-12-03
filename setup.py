e13babad3af2f1afb5c4af80df44f8e262dcfb78
Ahmad <zer0ne.io.x@gmail.com>
2024-12-03 17:26:59 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'cli_parser=cli_parser.parser:main',
+            'cli_parser=src.parser:main',
         ],
     },
 )
